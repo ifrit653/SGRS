b1104a4f720093fc1ef54f6c9b57c9ec9b7b1491
Figo <ifrit653@gmail.com>
2022-08-24 00:39:45 +0400

    error handling on login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'security.login', methods: ['GET', 'POST'])]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('pages/security/login.html.twig', [
             'controller_name' => 'SecurityController',
+            'last_username' => $lastUsername,
+            'error' => $error,
         ]);
     }
     #[Route('/logout', name: 'security.logout')]
